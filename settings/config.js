c41179ff6b71364bf7ded088e4b79e83031d9f57
zarifz01 <100981653+zarifz01@users.noreply.github.com>
2022-03-07 00:28:53 +0800

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="ZeeoneOfc"
-global.ownernumber = "62887435047326"
-global.botname = "Haruka-Bot"
+global.ownername ="ZrifOfc"
+global.ownernumber = "+1 (952) 522-3355"
+global.botname = "BoBot"
 global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'RIFQIBOTZ' //apikey
